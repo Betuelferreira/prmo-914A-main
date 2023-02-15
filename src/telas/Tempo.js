490079ba69c738d479871c0039ec2d774f8e3cb8
Júlio César Bomfim Ferreira <juliocesar.bomfim@hotmail.com>
2023-02-15 16:20:10 -0300

    replicação_temp

--- src/telas/Tempo.js
+++ src/telas/Tempo.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Image, StyleSheet, Text, ActivityIndicator } from 'react-native';
 import Txt from "../componentes/Texto";
 import Icone from '../componentes/Icone';
@@ -8,12 +8,12 @@ export default function Telas() {
   const [data, setData] = useState()
   const [isLoading, setLoading] = useState(true)
 
-  
-  useEffect(()=>{
-    fetch(url).then((res) => res.json()).then((data)=>{setData(data), setLoading(false)})
-  },[])
 
-  if(isLoading){
+  useEffect(() => {
+    fetch(url).then((res) => res.json()).then((data) => { setData(data), setLoading(false) })
+  }, [])
+
+  if (isLoading) {
     return <ActivityIndicator />
   }
 
@@ -32,33 +32,39 @@ export default function Telas() {
         <Txt negrito="bold" tamanho={22} texto="Next 7 days" cor="#FFFFFF"></Txt>
       </View>
       <View style={styles.card}>
-        <Image source={{uri:data ? `http://openweathermap.org/img/wn/${data.daily[0].weather[0].icon}@2x.png` : null}} style={{ marginRight: "10%", width: 30, height: 30 }} />
+        <Image source={{ uri: data ? `http://openweathermap.org/img/wn/${data.daily[0].weather[0].icon}@2x.png` : null }} style={{ marginRight: "10%", width: 30, height: 30 }} />
         <Txt negrito="bold" tamanho={20} texto="MACEIÓ, " cor="#FFFFFF"></Txt>
-        <Text>{data.daily[0].temp.max}</Text>
+        <Text>{data.daily[0].temp.day}</Text>
       </View>
       <View style={styles.card}>
-        <Image source={require("../imagens/sol.png")} style={{ marginRight: "10%", width: 30, height: 30 }} />
+        <Image source={{ uri: data ? `http://openweathermap.org/img/wn/${data.daily[1].weather[0].icon}@2x.png` : null }} style={{ marginRight: "10%", width: 30, height: 30 }} />
         <Txt negrito="bold" tamanho={20} texto="MACEIÓ, " cor="#FFFFFF"></Txt>
+        <Text>{data.daily[1].temp.day}</Text>
       </View>
       <View style={styles.card}>
-        <Image source={require("../imagens/sol.png")} style={{ marginRight: "10%", width: 30, height: 30 }} />
+        <Image source={{ uri: data ? `http://openweathermap.org/img/wn/${data.daily[2].weather[0].icon}@2x.png` : null }} style={{ marginRight: "10%", width: 30, height: 30 }} />
         <Txt negrito="bold" tamanho={20} texto="MACEIÓ, " cor="#FFFFFF"></Txt>
+        <Text>{data.daily[2].temp.day}</Text>
       </View>
       <View style={styles.card}>
-        <Image source={require("../imagens/sol.png")} style={{ marginRight: "10%", width: 30, height: 30 }} />
+        <Image source={{ uri: data ? `http://openweathermap.org/img/wn/${data.daily[3].weather[0].icon}@2x.png` : null }} style={{ marginRight: "10%", width: 30, height: 30 }} />
         <Txt negrito="bold" tamanho={20} texto="MACEIÓ, " cor="#FFFFFF"></Txt>
+        <Text>{data.daily[3].temp.day}</Text>
       </View>
       <View style={styles.card}>
-        <Image source={require("../imagens/sol.png")} style={{ marginRight: "10%", width: 30, height: 30 }} />
+        <Image source={{ uri: data ? `http://openweathermap.org/img/wn/${data.daily[4].weather[0].icon}@2x.png` : null }} style={{ marginRight: "10%", width: 30, height: 30 }} />
         <Txt negrito="bold" tamanho={20} texto="MACEIÓ, " cor="#FFFFFF"></Txt>
+        <Text>{data.daily[4].temp.day}</Text>
       </View>
       <View style={styles.card}>
-        <Image source={require("../imagens/sol.png")} style={{ marginRight: "10%", width: 30, height: 30 }} />
+        <Image source={{ uri: data ? `http://openweathermap.org/img/wn/${data.daily[5].weather[0].icon}@2x.png` : null }} style={{ marginRight: "10%", width: 30, height: 30 }} />
         <Txt negrito="bold" tamanho={20} texto="MACEIÓ, " cor="#FFFFFF"></Txt>
+        <Text>{data.daily[5].temp.day}</Text>
       </View>
       <View style={styles.card}>
-        <Image source={require("../imagens/sol.png")} style={{ marginRight: "10%", width: 30, height: 30 }} />
+        <Image source={{ uri: data ? `http://openweathermap.org/img/wn/${data.daily[6].weather[0].icon}@2x.png` : null }} style={{ marginRight: "10%", width: 30, height: 30 }} />
         <Txt negrito="bold" tamanho={20} texto="MACEIÓ, " cor="#FFFFFF"></Txt>
+        <Text>{data.daily[6].temp.day}</Text>
       </View>
 
     </View>
@@ -72,10 +78,10 @@ const styles = StyleSheet.create({
     margin: "5%"
   },
   header: {
-    maginTop: 5, 
-    flexDirection: "row", 
-    display: "flex", 
-    justifyContent: "center", 
+    maginTop: 5,
+    flexDirection: "row",
+    display: "flex",
+    justifyContent: "center",
     width: "100%"
   },
   temp: {
