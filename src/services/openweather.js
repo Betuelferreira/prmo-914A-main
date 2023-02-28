104797788127ffa34555881cd4f8ebf919a86603
Júlio César Bomfim Ferreira <juliocesar.bomfim@hotmail.com>
2023-02-28 09:08:22 -0300

    url tipo variavel

--- src/services/openweather.js
+++ src/services/openweather.js
@@ -1,3 +1,4 @@
 
 const apikey =  "d5bf8b3a01e1f7d835ce0f061df88c91";
-export default  url = `https://api.openweathermap.org/data/2.5/onecall?lat=-9.665&lon=-35.7353&units=metric&exclude=minute,hourly&appid=${apikey}`
+const url = `https://api.openweathermap.org/data/2.5/onecall?lat=-9.665&lon=-35.7353&units=metric&exclude=minute,hourly&appid=${apikey}`
+export default url
