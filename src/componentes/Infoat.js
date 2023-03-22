1314bfd11728fa59856059eb6b3a1c54b0792fc7
Alanz0ka <juliocesar.bomfim@hotmail.com>
2023-03-22 10:22:17 -0300

    primeira tela alteração

--- src/componentes/Infoat.js
+++ src/componentes/Infoat.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Image, Text, StyleSheet } from 'react-native';
+import { View, Image, Text, StyleSheet, ActivityIndicator } from 'react-native';
 import Txt from "../componentes/Texto";
 import Clima from '../componentes/informacao_clima';
 import url from '../services/openweather'
@@ -19,7 +19,24 @@ export default function info() {
 
   return (
     <View style={styles.container}>
-      <Image source={{ uri: data ? `http://openweathermap.org/img/wn/${data.daily['current'].weather[0].icon}http://openweathermap.org/img/wn/` : null }}/>
+      <Image source={{ uri: data ? `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@4x.png` : null }} style={styles.img} />
+      <Text style={styles.txt_description}>{data.current.weather[0].description}</Text>
+      <Text>{`${Date(data.current.dt)}`}</Text>
+      <Text style={styles.texto}>{`${(data.current.temp).toFixed(0)}°`}</Text>
+      <View style={styles.dados}>
+        <View style={styles.card}>
+          <Text>WIND</Text>
+        </View>
+        <View style={styles.card}>
+          <Text>FEELS LIKE</Text>
+        </View>
+        <View style={styles.card}>
+          <Text>INDEX UV</Text>
+        </View>
+        <View style={styles.card}>
+          <Text>PRESSURE</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -29,8 +46,39 @@ const styles = StyleSheet.create({
     marginTop: "5%",
     backgroundColor: 'blue',
     width: "80%",
-    height: "65%",
-    borderRadius: "20%",
+    height: 350,
+    borderRadius: "5%",
     alignItems: "center"
   },
+  img: {
+    width: 150,
+    height: 100,
+    marginTop: 20,
+  },
+  texto: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: "white",
+  },
+  txt_description: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: "white",
+    textTransform: 'capitalize'
+  },
+  dados: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap:'wrap',
+    borderRadius: 20,
+    borderColor: 'white',
+    width: '100%',
+    height: '32%',
+  },
+  card: {
+    width: '50%',
+    borderBottomWidth: 0,    
+    borderWidth: 0.5,
+    borderColor: 'white',
+  }
 })
--- src/telas/Login.js
+++ src/telas/Login.js
@@ -28,7 +28,7 @@ export default function Main() {
         
       </View>
       
-      <Lista/>
+      {/* <Lista/> */}
     </SafeAreaView>
     
     
